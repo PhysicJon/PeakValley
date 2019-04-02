24e9d0f9cda76f912797734e097b6dbdf597e778
unknown <j.r.alsop.18@gmail.com>
2019-04-02 17:25:53 +0100

    Adding test cases to renamed test file

--- PeakValley.Tests/PeakValleyDetectTests.cs
+++ PeakValley.Tests/PeakValleyDetectTests.cs
@@ -4,10 +4,14 @@ using PeakValleyAnalysis;
 
 namespace PeakValleyAnalysis.Tests
 {
-	public class UnitTest1
+	public class PeakValleyDetectTests
 	{
 		[Theory]
 		[InlineData(new int[] { 1, 2, 3, 4 }, new int[] { 2, 1, 1 })]
+		[InlineData(new int[] { }, new int[] { 0, 0, 0 })]
+		[InlineData(new int[] { 1 }, new int[] { 1, 1, 0 })]
+		[InlineData(new int[] { -1, -2, -3, -4 }, new int[] { 2, 1, 1 })]
+		[InlineData(new int[] { 1, 1, 2, 3, 4, 4 }, new int[] { 2, 1, 1 })]
 		public void PeakValleyDetect(int[] ints, int[] expected)
 		{
 			Assert.Equal(expected, new PeakValley().PeakValleyDetect(ints));
