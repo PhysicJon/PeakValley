fa62a9352a03f07f04dcc14da66df38859449e63
Jonathan Alsop <j.r.alsop.18@gmail.com>
2019-04-05 14:26:19 +0100

    Updating tests

--- PeakValley.Tests/PeakValleyDetectTests.cs
+++ PeakValley.Tests/PeakValleyDetectTests.cs
@@ -1,6 +1,4 @@
-using System;
 using Xunit;
-using PeakValleyAnalysis;
 
 namespace PeakValleyAnalysis.Tests
 {
@@ -12,6 +10,7 @@ namespace PeakValleyAnalysis.Tests
 		[InlineData(new int[] { 1 }, new int[] { 1, 1, 0 })]
 		[InlineData(new int[] { -1, -2, -3, -4 }, new int[] { 2, 1, 1 })]
 		[InlineData(new int[] { 1, 1, 2, 3, 4, 4 }, new int[] { 2, 1, 1 })]
+		[InlineData(new int[] { 1, 1, 2, 3, 4, 4,3,2,3,4,5,6,7,8,9,10,9,8,9,8,7,6,5,4,3,2,1,0 }, new int[] { 7, 3, 4 })]
 		public void PeakValleyDetect(int[] ints, int[] expected)
 		{
 			Assert.Equal(expected, new PeakValley().PeakValleyDetect(ints));
