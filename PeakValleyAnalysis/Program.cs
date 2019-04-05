90bfe7dd0b50333572b676ab845349a08ba4d0df
Jonathan Alsop <j.r.alsop.18@gmail.com>
2019-04-05 14:22:40 +0100

    Updating program init

--- PeakValleyAnalysis/Program.cs
+++ PeakValleyAnalysis/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
-
+using System.Linq;
 namespace PeakValleyAnalysis
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			int[] A = new int[] { 1, 2, 3, 4 };
-			new PeakValley().PeakValleyDetect(A);
+			int[] A = null;
+			
+			A = args.Length == 0 ?  new int[] { 1, 2, 3, 4 } : Array.ConvertAll(args[0].Split(',').ToArray(), s => int.Parse(s));
+			
+			var result = new PeakValley().PeakValleyDetect(A);
+			Console.WriteLine($"Total: {result[0]}, Peaks: {result[1]}, Valleys: {result[2]}");
 		}
 	}
 }
